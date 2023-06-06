560ac909ff8fb3e9a29298d762c012f0cf91764c
Me is gawasa <61944230+gawasa29@users.noreply.github.com>
2023-06-07 02:57:24 +0900

    feat: make UI  RoomMessageInput

--- lib/features/chat/presentation/chat_room_screen.dart
+++ lib/features/chat/presentation/chat_room_screen.dart
@@ -13,8 +13,48 @@ class ChatRoomScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
-        children: <Widget>[Text(userId)],
+        children: <Widget>[
+          Text(userId),
+          const Expanded(
+            child: SizedBox(),
+          ),
+          const _RoomMessageInput(),
+          Container(
+            height: MediaQuery.of(context).padding.bottom,
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class _RoomMessageInput extends StatelessWidget {
+  const _RoomMessageInput();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          icon: const Icon(Icons.image),
+          onPressed: () {},
+        ),
+        Expanded(
+          child: TextFormField(
+            minLines: 1,
+            maxLines: 5,
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(6),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+            ),
+          ),
+        ),
+        IconButton(
+          icon: const Icon(Icons.send),
+          onPressed: () {},
+        ),
+      ],
     );
   }
 }
