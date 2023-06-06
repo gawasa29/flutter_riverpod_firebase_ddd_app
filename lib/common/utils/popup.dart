65a08c4bc3d43c3ee5906ee249b61374dbe8422d
Me is gawasa <61944230+gawasa29@users.noreply.github.com>
2023-06-07 02:19:34 +0900

    fix: loaderPopup

--- lib/common/utils/popup.dart
+++ lib/common/utils/popup.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 
 void loaderPopup(context) {
   showDialog(
+    barrierColor: Colors.transparent,
     context: context,
     builder: (_) {
-      return const Dialog(
-        insetPadding: EdgeInsets.all(15),
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        child: Center(
-          child: CircularProgressIndicator(),
+      return const FractionallySizedBox(
+        heightFactor: 0.2,
+        widthFactor: 0.5,
+        child: Card(
+          color: Colors.transparent,
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
         ),
       );
     },
