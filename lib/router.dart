318e475cec3c60f152549ab5034408648f85c7e8
Me is gawasa <61944230+gawasa29@users.noreply.github.com>
2023-06-07 16:03:15 +0900

    refactor: change pageBuilder animation

--- lib/router.dart
+++ lib/router.dart
@@ -36,8 +36,8 @@ final routerProvider = Provider(
           GoRoute(
               path: '/UserList',
               name: UserListScreen.routeName,
-              pageBuilder: (context, state) => _buildPageWithAnimation(
-                    const UserListScreen(),
+              pageBuilder: (context, state) => const NoTransitionPage(
+                    child: UserListScreen(),
                   ),
               routes: [
                 GoRoute(
@@ -53,15 +53,15 @@ final routerProvider = Provider(
           GoRoute(
             path: '/ChatRoomList',
             name: ChatRoomListScreen.routeName,
-            pageBuilder: (context, state) => _buildPageWithAnimation(
-              const ChatRoomListScreen(),
+            pageBuilder: (context, state) => const NoTransitionPage(
+              child: ChatRoomListScreen(),
             ),
           ),
           GoRoute(
             path: '/Profile',
             name: ProfileScreen.routeName,
-            pageBuilder: (context, state) => _buildPageWithAnimation(
-              const ProfileScreen(),
+            pageBuilder: (context, state) => const NoTransitionPage(
+              child: ProfileScreen(),
             ),
           ),
         ],
@@ -69,25 +69,3 @@ final routerProvider = Provider(
     ],
   ),
 );
-
-CustomTransitionPage<void> _buildPageWithAnimation(Widget page) {
-  return CustomTransitionPage<void>(
-    child: page,
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      return FadeTransition(
-        opacity: animation,
-        child: SlideTransition(
-          position: animation.drive(
-            Tween<Offset>(
-              begin: const Offset(1, 0),
-              end: Offset.zero,
-            ).chain(
-              CurveTween(curve: Curves.easeIn),
-            ),
-          ),
-          child: child,
-        ),
-      );
-    },
-  );
-}
